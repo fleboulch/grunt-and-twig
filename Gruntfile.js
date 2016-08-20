d21f5b9831fc7bad8ea00c8a370c55fd95382ae9
florentL <florent.le.b@hotmail.fr>
2016-08-20 23:21:35 +0200

    Update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,114 +1,120 @@
 module.exports = function(grunt) {
-	
-	require('jit-grunt')(grunt);
-	
-	// new grunt-contrib-connect need serve-static
-	var serveStatic = require('serve-static');
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-	appName: 'app/',
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
-	copy: {
-	  main: {
-		files: [
-			{expand: true, cwd: '<%= appName %>', src: ['**/*.html'], dest: '<%= appName %>dist/'},
-			// includes files within path
-			//{expand: true, src: ['app/src/*'], dest: 'app/dist/', filter: 'isFile'},
+    require('jit-grunt')(grunt);
 
-			// includes files within path and its sub-directories
-			//{expand: true, src: ['app/src'], dest: 'app/dist/'},
+    // new grunt-contrib-connect need serve-static
+    var serveStatic = require('serve-static');
 
-			// makes all src relative to cwd
-			//{expand: true, cwd: '<%= appName %>scss/', src: ['**'], dest: '<%= appName %>dist/scss/'},
-
-			// flattens results to a single level
-			//{expand: true, flatten: true, src: ['path/**'], dest: 'dist/', filter: 'isFile'},
-		],
-	  },
-	},
-	clean: {
-      folder: ["<%= appName %>dist/", ".tmp/"]
-	},
-	compass: {                  // Task
-		dist: {
-		  options: {
-			sassDir: '<%= appName %>scss/',
-			cssDir: '<%= appName %>dist/css/'
-		  }
-		}
-	},
-	connect: {
-		options: {
-		  port: 9001, // configure your port here
-		  hostname: 'localhost',
-		  //base: '<%= appName %>dist', // configure your site distribution path here
-		  //open: true,
-		  //keepalive: true,
-		  livereload: 35729
-		},
-		livereload: {
-			options: {
-			  open: true,
-			  base: '<%= appName %>dist/',
-			  middleware: function (connect) {
-				return [
-				  serveStatic('app/dist')
-				   //connect.static('app/dist')
-				];
-			  }
-			}
-		},
-		dist: {
-			options: {
-			  open: true,
-			  base: 'app/dist'
-			}
-		}
-	},
-	watch: {
-	  /*html: {
-		files: '<%= appName %>dist/*.html',
-		//options: {
-		  //livereload: '<%= connect.options.livereload %>'
-		//}
-	  },*/
-	  livereload: {
-        options: {
-          livereload: '<%= connect.options.livereload %>'
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        appName: 'app/',
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
         },
-        files: [ 			   
-          'app/dist/*.html'
-          
-        ]
-	  }
-	}
-  });
+        copy: {
+            test: {
+                files: [
+                    // includes files within path
+                    //{expand: true, src: ['app/src/*'], dest: 'app/dist/', filter: 'isFile'},
 
-  // Load the plugin that provides the "uglify" task.
-  //grunt.loadNpmTasks('grunt-contrib-uglify');
-  //grunt.loadNpmTasks('grunt-contrib-copy');
+                    // includes files within path and its sub-directories
+                    //{expand: true, src: ['app/src'], dest: 'app/dist/'},
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-  
-  // custom task(s)
-  
-  grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
-    
-    grunt.task.run([
-      'connect:livereload',
-      'watch'
-    ]);
-});
+                    // makes all src relative to cwd
+                    {expand: true, cwd: '<%= appName %>scss/', src: ['**'], dest: '<%= appName %>dist/scss/'},
 
+                    // flattens results to a single level
+                    //{expand: true, flatten: true, src: ['path/**'], dest: 'dist/', filter: 'isFile'},
+                ],
+            },
+            html: {
+                files: [
+                    {expand: true, cwd: '<%= appName %>', src: ['*.html'], dest: '<%= appName %>dist/'},
+                ],
+            }
+        },
+        clean: {
+            folder: ["<%= appName %>dist/", ".tmp/"]
+        },
+        compass: {                  // Task
+            dist: {
+                options: {
+                    sassDir: '<%= appName %>scss/',
+                    cssDir: '<%= appName %>dist/css/'
+                }
+            }
+        },
+        connect: {
+            options: {
+                port: 9001, // configure your port here
+                hostname: 'localhost',
+                //base: '<%= appName %>dist', // configure your site distribution path here
+                //open: true,
+                //keepalive: true,
+                livereload: 35729
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    base: '<%= appName %>dist/',
+                    middleware: function (connect) {
+                        return [
+                            serveStatic('app/dist')
+                            //connect.static('app/dist')
+                        ];
+                    }
+                }
+            },
+            dist: {
+                options: {
+                    open: true,
+                    base: 'app/dist'
+                }
+            }
+        },
+        watch: {
+            html: {
+                files: [
+                    '<%= appName %>index.html',
+                ],
+                tasks: ['copy:html']
+                //options: {
+                //livereload: '<%= connect.options.livereload %>'
+                //}
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    'app/dist/index.html'
+
+                ]
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    //grunt.loadNpmTasks('grunt-contrib-uglify');
+    //grunt.loadNpmTasks('grunt-contrib-copy');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+
+    // custom task(s)
+
+    grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
+
+        grunt.task.run([
+            'connect:livereload',
+            'watch'
+        ]);
+    });
 };
