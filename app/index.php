5c96d9a919fc79a05ecfbabca5cedc07bee14791
florentL <florent.le.b@hotmail.fr>
2016-08-30 01:50:28 +0200

    fix path

--- app/index.php
+++ app/index.php
@@ -2,7 +2,7 @@
 
 require_once(__DIR__."/../vendor/autoload.php");
 
-$loader = new \Twig_Loader_Filesystem(__DIR__.'\views');
+$loader = new \Twig_Loader_Filesystem(__DIR__.'/views');
 $twig = new \Twig_Environment($loader);
 
 echo $twig->render('base.html.twig', [
